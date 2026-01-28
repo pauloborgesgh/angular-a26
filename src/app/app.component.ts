5101648d7343d6f6a84f494b8727e09954f38498
borgesgh <pauloborgeszambon@gmail.com>
2026-01-27 21:08:39 -0300

    add new component calc number v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { MainComponent } from './main/main.component';
+import { CalculadoraComponent } from './calculadora/calculadora.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent,MainComponent],
+  imports: [RouterOutlet, HeaderComponent,MainComponent,CalculadoraComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
